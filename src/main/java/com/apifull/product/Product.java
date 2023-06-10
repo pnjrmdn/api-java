2e2dea0fafac1694a42cbe243d4303d9d9631d16
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-06-11 03:29:36 +0800

    update commit

--- src/main/java/com/apifull/product/Product.java
+++ src/main/java/com/apifull/product/Product.java
@@ -1,77 +1,64 @@
-package com.api.models.entities;
+package com.apifull.product;
 
-import java.io.Serializable;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name="product")
-public class Product implements Serializable{
-
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
 
-    private String  name;
-    private String  description;
-    private double  quantity;
-    private double  price;
-
+    
     public Product() {
+        
     }
 
-    public Product(Long id, String name, String description, double quantity, double price) {
+    public Product(Long id, String name, String description, double price, int quantity) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.quantity = quantity;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(double quantity) {
-        this.quantity = quantity;
-    }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-
+    
 }
+
